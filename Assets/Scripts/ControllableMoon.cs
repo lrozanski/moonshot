ec81590090b47fe1e84afbf9f29672dd7a6bd76d
Lech Różański <l.rozanski88@gmail.com>
2020-11-17 20:59:41 +0100

    Initial commit

---
+++ Assets/Scripts/ControllableMoon.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class ControllableMoon : MonoBehaviour {
+
+    [SerializeField]
+    private LineRenderer forceLine;
+
+    [SerializeField]
+    private float maxLength;
+
+    private bool _selected;
+    private Vector2 _aimVector;
+    private Rigidbody2D _rigidbody2D;
+
+    private void Start() {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnMouseDown() {
+        _selected = true;
+        forceLine.enabled = true;
+    }
+
+    private void OnMouseUp() {
+        if (_selected) {
+            forceLine.enabled = false;
+            _rigidbody2D.AddForce(_aimVector, ForceMode2D.Impulse);
+        }
+        _selected = false;
+    }
+
+    private void Update() {
+        if (!_selected) {
+            return;
+        }
+        var position = transform.position;
+        var mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mouseWorldPos.z = position.z;
+
+        var vectorToPlanet = position - mouseWorldPos;
+        var directionToPlanet = vectorToPlanet.normalized;
+        if (vectorToPlanet.magnitude > maxLength) {
+            vectorToPlanet = directionToPlanet * maxLength;
+        }
+        _aimVector = vectorToPlanet;
+
+        forceLine.SetPositions(new[] {
+            position,
+            position - vectorToPlanet
+        });
+        forceLine.endWidth = vectorToPlanet.magnitude * 0.2f;
+    }
+}
