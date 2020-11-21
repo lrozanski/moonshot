a0f14744264e6a4fb6fb790050b165e292bf5c52
Lech Różański <l.rozanski88@gmail.com>
2020-11-21 21:06:07 +0100

    Added victory message; target moon properly scales with the number of moons

---
+++ Assets/Scripts/Canvas.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Canvas : MonoBehaviour {
+
+    [SerializeField]
+    private Moon moon;
+
+    [SerializeField]
+    private GameObject victoryPanel;
+
+    private void Start() {
+        moon.onSuperMoonAchieved += () => victoryPanel.SetActive(true);
+    }
+}
--- Assets/Scripts/Moon.cs
+++ Assets/Scripts/Moon.cs
@@ -2,12 +2,32 @@
 
 public class Moon : MonoBehaviour {
 
+    public delegate void SuperMoonAchieved();
+
+    public event SuperMoonAchieved onSuperMoonAchieved;
+
+    [SerializeField]
+    private float maxScaleIncrease;
+
+    [SerializeField]
+    private float scalePerMoon;
+
     private void OnCollisionEnter2D(Collision2D other) {
         if (!other.gameObject.CompareTag("Controllable Moon")) {
             return;
         }
-        transform.localScale += (Vector3) (Vector2.one * 0.1f);
+        transform.localScale += (Vector3) (Vector2.one * scalePerMoon);
 
         Destroy(other.gameObject);
+
+        if (GameObject.FindGameObjectsWithTag("Controllable Moon").Length == 1) {
+            onSuperMoonAchieved?.Invoke();
+        }
+    }
+
+    private void Start() {
+        var controllableMoons = GameObject.FindGameObjectsWithTag("Controllable Moon").Length;
+
+        scalePerMoon = maxScaleIncrease / controllableMoons;
     }
 }
