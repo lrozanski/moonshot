776d7c83c93e0ef2afacb3044033309b8b149432
Lech Różański <l.rozanski88@gmail.com>
2020-11-23 21:42:48 +0100

    Improved gravity and movement, introduced turns

---
+++ Assets/Scripts/Editor/ColliderTools.cs
@@ -0,0 +1,35 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Editor {
+    public static class ColliderTools {
+
+        private const int MaxVertices = 16;
+
+        [MenuItem("Tools/Create Moon Collider")]
+        public static void CreateMoonCollider() {
+            var currentGameObject = Selection.activeGameObject;
+            if (currentGameObject == null || !currentGameObject.CompareTag("Controllable Moon")) {
+                return;
+            }
+            var circleCollider = currentGameObject.GetComponent<CircleCollider2D>();
+            if (circleCollider == null) {
+                Debug.Log("A CircleCollider2D is required in order to determine collider radius");
+                return;
+            }
+            var collider = currentGameObject.AddComponent<PolygonCollider2D>();
+
+            var points = new Vector2[MaxVertices];
+            for (var i = 0; i < MaxVertices; i++) {
+                var angle = Mathf.PI * 2f / MaxVertices * i;
+
+                points[i] = new Vector2(
+                    Mathf.Cos(angle) * circleCollider.radius,
+                    Mathf.Sin(angle) * circleCollider.radius
+                );
+                Debug.Log($"Angle: {angle}, point: {points[i]}");
+            }
+            collider.SetPath(0, points);
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager Instance { get; private set; }
+
+    public int Shots { get; private set; }
+
+    public bool CanShoot { get; private set; }
+
+    private List<Rigidbody2D> _controllableMoons;
+
+    private void Start() {
+        Instance = this;
+        _controllableMoons = GameObject.FindGameObjectsWithTag("Controllable Moon")
+            .Select(moon => moon.GetComponent<Rigidbody2D>())
+            .ToList();
+    }
+
+    private void Update() {
+        var moving = _controllableMoons.Any(moon => moon.bodyType != RigidbodyType2D.Kinematic);
+        if (moving) {
+            CanShoot = false;
+        }
+
+        if (!CanShoot && !moving) {
+            EndTurn();
+        }
+    }
+
+    public void EndTurn() {
+        CanShoot = true;
+        Shots++;
+        
+        Debug.Log("End turn");
+    }
+}
