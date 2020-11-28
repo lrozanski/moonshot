88497b8ae04c7665e7ae70f84114110b5a21c370
Lech Różański <l.rozanski88@gmail.com>
2020-11-28 15:11:28 +0100

    Added par and the number of shots; fixed turns

--- Assets/Scripts/ControllableMoon.cs
+++ Assets/Scripts/ControllableMoon.cs
@@ -17,7 +17,7 @@ public class ControllableMoon : MonoBehaviour {
 
     [field: SerializeField]
     public float DistanceTraveled { get; private set; }
-    
+
     private bool _selected;
     private Vector2 _aimVector;
     private Rigidbody2D _rigidbody2D;
@@ -42,6 +42,8 @@ public class ControllableMoon : MonoBehaviour {
             _rigidbody2D.bodyType = RigidbodyType2D.Dynamic;
             forceLine.enabled = false;
             _rigidbody2D.AddForce(_aimVector * forceMultiplier, ForceMode2D.Impulse);
+
+            GameManager.Instance.StartLevel();
         }
         _selected = false;
     }
@@ -103,13 +105,15 @@ public class ControllableMoon : MonoBehaviour {
     }
 
     private void LateUpdate() {
-        if (DistanceTraveled > 0f && PositionDelta < 0.001f) {
-            _rigidbody2D.bodyType = RigidbodyType2D.Kinematic;
-            _rigidbody2D.velocity = Vector2.zero;
-            Physics2D.SyncTransforms();
-
-            _startPosition = _previousPosition;
-            DistanceTraveled = 0f;
+        if (!(DistanceTraveled > 0f) || !(PositionDelta < 0.002f)) {
+            return;
         }
+        _rigidbody2D.bodyType = RigidbodyType2D.Kinematic;
+        _rigidbody2D.velocity = Vector2.zero;
+        _rigidbody2D.angularVelocity = 0f;
+        Physics2D.SyncTransforms();
+
+        _startPosition = _previousPosition;
+        DistanceTraveled = 0f;
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
@@ -7,19 +8,33 @@ public class GameManager : MonoBehaviour {
     public static GameManager Instance { get; private set; }
 
     public int Shots { get; private set; }
-
+    
     public bool CanShoot { get; private set; }
 
     [SerializeField]
     private Moon targetMoon;
 
+    [SerializeField]
+    private int par;
+
+    [SerializeField]
+    private TextMeshProUGUI shotsText;
+
+    [SerializeField]
+    private TextMeshProUGUI parText;
+
     private List<Rigidbody2D> _controllableMoons;
+
+    private bool _started;
 
     private void Start() {
         Instance = this;
         _controllableMoons = GameObject.FindGameObjectsWithTag("Controllable Moon")
             .Select(moon => moon.GetComponent<Rigidbody2D>())
             .ToList();
+
+        parText.text = $"Par: {par}";
+        CanShoot = true;
     }
 
     private void OnEnable() {
@@ -31,7 +46,10 @@ public class GameManager : MonoBehaviour {
     }
 
     private void Update() {
-        var moving = _controllableMoons.Any(moon => moon.bodyType != RigidbodyType2D.Kinematic);
+        if (!_started) {
+            return;
+        }
+        var moving = _controllableMoons.Any(moon => moon != null && moon.bodyType != RigidbodyType2D.Kinematic);
         if (moving) {
             CanShoot = false;
         }
@@ -41,11 +59,16 @@ public class GameManager : MonoBehaviour {
         }
     }
 
+    public void StartLevel() {
+        _started = true;
+    }
+
     public void EndTurn() {
         CanShoot = true;
         Shots++;
 
-        Debug.Log("End turn");
+        shotsText.text = $"Shots: {Shots}";
+        Debug.Log($"End turn. Shots: {Shots}");
     }
 
     private void RemoveMoon(Rigidbody2D moonRigidBody2D) {
